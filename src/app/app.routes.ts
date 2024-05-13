a87f9b3867fe8c2b905dda58eacf522ac6acdaaf
Erick <eefeaga2000@gmail.com>
2024-05-12 21:48:56 -0300

    feat: cadastro de nova rota para componente de gerenciamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ManagementComponent } from './components/management/management.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-    }
+        children: [
+            {
+                path: 'controle',
+                component: ManagementComponent
+            }
+        ]
+    },
 ];
