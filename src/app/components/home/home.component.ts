13eba1c1038d3c32248c44f8b49c4f2b935e77b5
Erick <eefeaga2000@gmail.com>
2024-05-12 21:49:26 -0300

    feat: implementacao de router-outlet no componente home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSidenavModule, NavbarComponent],
+  imports: [RouterOutlet, MatSidenavModule, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
