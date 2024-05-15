cece9ffb655778e3e464084812ce40a44efd0947
Erick <eefeaga2000@gmail.com>
2024-05-15 20:42:54 -0300

    feat: create category list

---
+++ src/app/consts/categories.const.ts
@@ -0,0 +1,15 @@
+export const categories = [
+    { id: 1, name: 'Alimentação', description: 'Compras de supermercado, refeições em restaurantes, delivery de comida, lanches, etc' },
+    { id: 2, name: 'Caridade e Doações', description: 'Contribuições para organizações sem fins lucrativos, igrejas, campanhas de crowdfunding, etc' },
+    { id: 3, name: 'Despesas Pessoais', description: 'Despesas diversas que não se enquadram em outras categorias, como presentes, cuidados com animais de estimação, serviços pessoais, etc' },
+    { id: 4, name: 'Dívidas e Empréstimos', description: 'Pagamentos de cartão de crédito, parcelas de empréstimos, juros, financiamentos, etc' },
+    { id: 5, name: 'Economia e Investimento', description: 'Contribuições para aposentadoria, investimentos em ações, fundos, poupança, etc' },
+    { id: 6, name: 'Educação', description: 'Mensalidades escolares, material didático, cursos, aulas particulares, livros, etc' },
+    { id: 7, name: 'Entretenimento', description: 'Cinema, teatro, shows, assinaturas de streaming, livros, jogos, eventos esportivos, viagens, hobbies, etc' },
+    { id: 8, name: 'Impostos e Taxas', description: 'Pagamentos de impostos, taxas de serviços, licenças, multas, etc' },
+    { id: 9, name: 'Moradia', description: 'Aluguel ou pagamento de hipoteca, contas de água, luz, gás, internet, telefone, manutenção residencial, etc' },
+    { id: 10, name: 'Roupas e Acessórios', description: 'Compras de vestuário, calçados, acessórios pessoais, produtos de beleza, etc' },
+    { id: 11, name: 'Saúde', description: 'Consultas médicas, medicamentos, tratamentos, seguros de saúde, academias, produtos de higiene, etc' },
+    { id: 12, name: 'Transferência', description: 'Transferência bancária, PIX, TED, etc'},
+    { id: 13, name: 'Transporte', description: 'Combustível, transporte público, táxis, serviços de compartilhamento de carros, manutenção de veículos, estacionamento, pedágios, etc' },
+]
