d831fa88f6ab037807f3a0a75e57a8e3005a7e89
Erick <eefeaga2000@gmail.com>
2024-05-17 19:29:42 -0300

    feat: using total-balance component on home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { TotalBalanceComponent } from '../total-balance/total-balance.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, NavbarComponent],
+  imports: [RouterOutlet, MatSidenavModule, NavbarComponent, MatCardModule, TotalBalanceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
