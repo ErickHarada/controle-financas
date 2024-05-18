a0683d42ec6270b38cd99c9f6090e225683dee8d
Erick <eefeaga2000@gmail.com>
2024-05-17 21:41:59 -0300

    feat: consuming table component

--- src/app/components/management/management.component.ts
+++ src/app/components/management/management.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CreateRegistryComponent } from '../create-registry/create-registry.component';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-management',
   standalone: true,
-  imports: [CreateRegistryComponent],
+  imports: [CreateRegistryComponent, TableComponent],
   templateUrl: './management.component.html',
   styleUrl: './management.component.scss'
 })
