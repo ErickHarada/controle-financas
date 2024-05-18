0a6eae8aebf134020fe17995ab5437d44c0cae65
Erick <eefeaga2000@gmail.com>
2024-05-17 21:42:44 -0300

    feat: listening type field value changes

--- src/app/components/create-registry/create-registry.component.ts
+++ src/app/components/create-registry/create-registry.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -6,9 +6,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
-import { categories } from '../../consts/categories.const';
-import { CommonModule, NgFor } from '@angular/common';
+import { expenseCategories, incomeCategories } from '../../consts/categories.const';
+import { NgFor } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { RegistryType } from '../../enums/registry-type.enum';
 
 @Component({
   selector: 'app-create-registry',
@@ -27,13 +28,23 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './create-registry.component.html',
   styleUrl: './create-registry.component.scss'
 })
-export class CreateRegistryComponent {
+export class CreateRegistryComponent implements OnInit {
 
-  categoryList = categories;
+  categoryList = [];
   createRegistryForm: FormGroup;
 
   constructor(private readonly formBuilder: FormBuilder) {
     this.createRegistryForm = this.buildForm();
+  }
+
+  ngOnInit(): void {
+    this.subscribeTypeValue();
+  }
+
+  subscribeTypeValue(): void {
+    this.createRegistryForm.get('type').valueChanges.subscribe((value: number) => {
+      this.categoryList = value === RegistryType.INCOME ? incomeCategories : expenseCategories
+    })
   }
 
   create(): void { }
