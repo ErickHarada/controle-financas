1e6ddec20dc84c29e5c3b6736030006ccea4b7a9
Erick <eefeaga2000@gmail.com>
2024-05-17 21:40:52 -0300

    feat: categories list

--- src/app/consts/categories.const.ts
+++ src/app/consts/categories.const.ts
@@ -1,4 +1,4 @@
-export const categories = [
+export const expenseCategories = [
     { id: 1, name: 'Alimentação', description: 'Compras de supermercado, refeições em restaurantes, delivery de comida, lanches, etc' },
     { id: 2, name: 'Caridade e Doações', description: 'Contribuições para organizações sem fins lucrativos, igrejas, campanhas de crowdfunding, etc' },
     { id: 3, name: 'Despesas Pessoais', description: 'Despesas diversas que não se enquadram em outras categorias, como presentes, cuidados com animais de estimação, serviços pessoais, etc' },
@@ -10,6 +10,13 @@ export const categories = [
     { id: 9, name: 'Moradia', description: 'Aluguel ou pagamento de hipoteca, contas de água, luz, gás, internet, telefone, manutenção residencial, etc' },
     { id: 10, name: 'Roupas e Acessórios', description: 'Compras de vestuário, calçados, acessórios pessoais, produtos de beleza, etc' },
     { id: 11, name: 'Saúde', description: 'Consultas médicas, medicamentos, tratamentos, seguros de saúde, academias, produtos de higiene, etc' },
-    { id: 12, name: 'Transferência', description: 'Transferência bancária, PIX, TED, etc'},
+    { id: 12, name: 'Transferência', description: 'Transferência bancária, PIX, TED, etc' },
     { id: 13, name: 'Transporte', description: 'Combustível, transporte público, táxis, serviços de compartilhamento de carros, manutenção de veículos, estacionamento, pedágios, etc' },
+]
+
+export const incomeCategories = [
+    { id: 14, name: 'Salário', description: 'Salário base, Horas extras, Bônus, Comissões' },
+    { id: 15, name: 'Rendimentos de Investimentos', description: 'Dividendos, Juros de Poupança, Ganhos de Capital, Aluguel de Propriedades' },
+    { id: 16, name: 'Receitas de Negócios', description: 'Vendas de Produtos, Prestação de Serviços, Parcerias / Joint Ventures, Royalties' },
+    { id: 17, name: 'Outras Receitas', description: 'Reembolsos, Prêmios, Aposentadoria / Pensão, Empréstimos Recebidos, Doações Recebidas, Vendas de Ativos, Heranças' },
 ]
