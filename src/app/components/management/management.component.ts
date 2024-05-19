b27e301ffeced1f341eac3f28760191c58b5d07a
Erick <eefeaga2000@gmail.com>
2024-05-19 14:18:04 -0300

    refactor: change folder name

--- src/app/components/management/management.component.ts
+++ src/app/components/management/management.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CreateRegistryComponent } from '../create-registry/create-registry.component';
-import { RegistryTableComponent } from '../table/registry-table.component';
+import { RegistryTableComponent } from '../registry-table/registry-table.component';
 
 @Component({
   selector: 'app-management',
