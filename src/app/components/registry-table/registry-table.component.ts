5e9cf1796d3af51a9b8a0136cfb5a09ea588a90a
Erick <eefeaga2000@gmail.com>
2024-05-19 15:08:26 -0300

    refactor: refactor registry-table

--- src/app/components/registry-table/registry-table.component.ts
+++ src/app/components/registry-table/registry-table.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { RegistryTable } from '../../interfaces/registry-table.interface';
@@ -11,10 +10,10 @@ import { RegistryTable } from '../../interfaces/registry-table.interface';
   styleUrl: './registry-table.component.scss'
 })
 export class RegistryTableComponent {
-  @Input() dataSource: RegistryTable[] =  [
+  @Input() dataSource: RegistryTable[] = [
     { type: 'Entrada', category: 'Salário', value: '3000' },
     { type: 'Saída', category: 'Alimentação', value: '32.45' },
     { type: 'Saída', category: 'Entretenimento', value: '80.50' },
   ];
-  displayedColumns: string[] = ['type', 'category', 'value'];
+  displayedColumns = ['type', 'category', 'value'];
 }
