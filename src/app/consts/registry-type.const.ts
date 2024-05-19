74d7e10e85240d059c4f2b95c21d7710aed204b7
Erick <eefeaga2000@gmail.com>
2024-05-19 15:07:31 -0300

    feat: create registry type const

---
+++ src/app/consts/registry-type.const.ts
@@ -0,0 +1,6 @@
+import { RegistryType } from "../enums/registry-type.enum";
+
+export const registryType = [
+    { id: RegistryType.INCOME, name: 'Entrada'},
+    { id: RegistryType.EXPENSE, name: 'Saída'},
+]
