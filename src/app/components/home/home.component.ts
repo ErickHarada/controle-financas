4aa042b06938aff27b7952a2fd4cd506f8d20632
Erick <eefeaga2000@gmail.com>
2024-05-19 21:57:06 -0300

    feat: add month chart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { TotalBalanceComponent } from '../total-balance/total-balance.component';
+import { Month } from '../../enums/month.enum';
 
 @Component({
   selector: 'app-home',
@@ -13,5 +14,5 @@ import { TotalBalanceComponent } from '../total-balance/total-balance.component'
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  month = Month[new Date().getMonth() + 1]
 }
