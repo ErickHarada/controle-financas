9124f2538f010992db2ed9fc55026fe45ca020a5
Erick <eefeaga2000@gmail.com>
2024-05-22 19:11:59 -0300

    feat: add expansion panel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,14 +5,17 @@ import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { TotalBalanceComponent } from '../total-balance/total-balance.component';
 import { Month } from '../../enums/month.enum';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, NavbarComponent, MatCardModule, TotalBalanceComponent],
+  imports: [RouterOutlet, MatSidenavModule, NavbarComponent, MatCardModule, TotalBalanceComponent, MatExpansionModule, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  month = Month[new Date().getMonth() + 1]
+  month = Month[new Date().getMonth() + 1];
+  monthList = [];
 }
